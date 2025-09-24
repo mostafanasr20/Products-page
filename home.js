db65788a1b06b884c8bd8bbdceed24704bd22aa4
Mostafa Nasr <mostafnasr654@gmail.com>
2025-09-24 15:48:23 +0300

    New section

--- home.js
+++ home.js
@@ -11,11 +11,11 @@ window.addEventListener("load", function() {
 
     if(header.style.top == 0){
       header.style.background = "transparent"
-      header.style.position = "fixed"
     
     }
-
 });
+
+
 
 
 const slides = document.querySelectorAll(".landing .box");
@@ -27,10 +27,7 @@ function showSlide() {
   index = (index + 1) % slides.length; 
 }
 
-// أول بوكس ظاهر
 showSlide();
-
-// يغير كل 3 ثواني
 setInterval(showSlide, 3000);
 
 
@@ -38,11 +35,90 @@ let links = document.querySelector(".links");
     let menu = document.querySelector(".menu");
 
     menu.onclick = function(){
-     if (links.style.right === "-1000px" || !links.style.right){
-        links.style.right = "20px"
+     if (links.style.opacity === "0" || !links.style.opacity){
+        links.style.opacity = 1
     }else{
-        links.style.right = "-1000px"
+        links.style.opacity = 0
     }
     }
-    console.log(menu)
+
+   let link = document.querySelectorAll(".links div");
+let cards = document.querySelectorAll(".cards-container .card");
+
+link.forEach((div) => {
+    div.addEventListener("click" , removeActive)
+    div.addEventListener("click" , manageBox)
+})
+
+function removeActive(){
+    link.forEach((div) => {
+        div.classList.remove("active");
+        this.classList.add("active");
+    })
+}
+
+function manageBox(){
+    cards.forEach((card) => {
+        card.style.display = "none";
+    })
+  
+document.querySelectorAll(this.dataset.dog).forEach((ele) => {
+    ele.style.display = "block";
+})
+}
+
+
+
+//   cards.forEach(card => {
+//     if (window.scrollY >= card.offsetTop) {
+//       card.style.opacity = 2;
+//     } 
+
+//   });
+// };
+
+
+
+// let observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.style.opacity = 1;
+//     } else {
+//       entry.target.style.opacity = 0;
+//     }
+//   });
+// }, {
+//   threshold: 0.1 // يعني لازم 10% من الكارد يظهر عشان يتفعل
+// });
+
+// cards.forEach(card => observer.observe(card));
+
+let serves = new IntersectionObserver(carr => {
+    carr.forEach(ele => {
+        if(ele.isIntersecting){
+            ele.target.style.opacity = 1
+        }else{
+            ele.target.style.opacity = 0
+        }
+    })
+   threshold: 0.1 
+})
+cards.forEach(card => serves.observe(card) )
+
+
+let boxes = document.querySelectorAll(".boxes-cont")
+
+let loop = new IntersectionObserver (loops =>{
+    loops.forEach(e =>{
+        if(e.isIntersecting){
+            e.target.style.opacity = 1 
+        }
+         else{
+            ele.target.style.opacity = 0
+        }
+    })
+     threshold: 1.1 
+} )
+
+boxes.forEach(card => serves.observe(card) )
 
